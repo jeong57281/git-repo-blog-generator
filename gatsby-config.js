3166371c4975ac73be7434913d6d4f6d48245d9f
jeong57281 <jeong5728@gmail.com>
2023-01-17 11:59:57 +0900

    build: gatsby-source-filesystem 패키지 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,16 +4,23 @@ module.exports = {
     description: `A simple blog starter using Typescript in front & back side with eslint & prettier.`,
     author: {
       name: `Christian`,
-      content: `I like build some things using TypeScript`
+      content: `I like build some things using TypeScript`,
     },
     social: {
-      github: `https://github.com/Lindeneg`
-    }
+      github: `https://github.com/Lindeneg`,
+    },
   },
   pathPrefix: `${require('git-repo-name').sync()}/git-algo-repo-summary`,
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: __dirname,
+        ignore: [`**/\.*`, `node_modules`],
+      },
+    },
     {
       resolve: 'gatsby-plugin-material-ui',
       // If you want to use styled components you should change the injection order.
@@ -21,13 +28,13 @@ module.exports = {
         // stylesProvider: {
         //   injectFirst: true,
         // },
-      }
+      },
     },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
-        component: require.resolve(`./src/components/layout/index.tsx`)
-      }
+        component: require.resolve(`./src/components/layout/index.tsx`),
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -38,11 +45,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    }
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ]
+  ],
 };
