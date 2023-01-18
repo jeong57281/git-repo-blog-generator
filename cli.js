2a57db1472ff58aa6dcf7ae026a7ae12a95d4dd8
jeong57281 <jeong5728@gmail.com>
2023-01-18 16:02:00 +0900

    feat: cli program 빌드 시 에러처리

--- cli.js
+++ cli.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process');
+const chalk = require('chalk');
 
-spawn('gatsby', ['build', '--prefix-paths'], { stdio: 'inherit', cwd: __dirname });
+const buildCmd = spawn('gatsby', ['build', '--prefix-paths'], {
+  stdio: 'inherit',
+  cwd: __dirname,
+});
+
+buildCmd.on('error', (err) => {
+  if (err.code === 'ENOENT') {
+    console.log(chalk.yellow('gatsby-cli'), 'not found');
+  }
+});
+
+/*
+spawn('gatsby', ['develop'], {
+  stdio: 'inherit',
+  cwd: __dirname,
+});
+*/
