65e96cc8f4cd0966883cb6a5d15e44b68cc4f575
jeong57281 <jeong5728@gmail.com>
2023-01-20 17:24:32 +0900

    feat: cli 프로그램 'build', 'develop' argv 에 따라 분기

--- cli.js
+++ cli.js
@@ -3,20 +3,30 @@
 const { spawn } = require('child_process');
 const chalk = require('chalk');
 
-const buildCmd = spawn('gatsby', ['build', '--prefix-paths'], {
-  stdio: 'inherit',
-  cwd: __dirname,
-});
+if (process.argv[2] === 'build') {
+  const buildCmd = spawn('gatsby', ['build', '--prefix-paths'], {
+    stdio: 'inherit',
+    cwd: __dirname,
+  });
 
-buildCmd.on('error', (err) => {
-  if (err.code === 'ENOENT') {
-    console.log(chalk.yellow('gatsby-cli'), 'not found');
-  }
-});
+  buildCmd.on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      console.log(chalk.yellow('gatsby-cli'), 'not found');
+    }
+  });
+} else if (process.argv[2] === 'develop') {
+  const devCmd = spawn('gatsby', ['develop'], {
+    stdio: 'inherit',
+    cwd: __dirname,
+  });
 
-/*
-spawn('gatsby', ['develop'], {
-  stdio: 'inherit',
-  cwd: __dirname,
-});
-*/
+  devCmd.on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      console.log(chalk.yellow('gatsby-cli'), 'not found');
+    }
+  });
+} else {
+  console.log(
+    `\nusage: $ ${chalk.yellow('grbgen')} build ${chalk.gray('or')} develop\n`
+  );
+}
