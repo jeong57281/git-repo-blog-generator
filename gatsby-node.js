a9dcc90edeb8bce460c0a575f088232cc5c364bc
jeong57281 <jeong5728@gmail.com>
2023-01-22 15:54:33 +0900

    fix: 배포 시 .cache 폴더 babel transpiling 되지 않는 오류 수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,39 @@ const gitDateExtractor = require('git-date-extractor');
 const { getCliCwd } = require('./src/util/process.js');
 const { setPluginOptionsDynamically } = require('./src/util/gatsby.js');
 
+exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
+  const config = getConfig();
+
+  /**
+   * 해당 패키지는 패키지에 포함된 소스와 gatsby를 이용하여 빌드하기 때문에
+   * node_modules 폴더 내에 public 과 .cache 폴더가 생겨난다.
+   *
+   * 그 중 .cache 폴더 내 파일들은 babel에 의해 transpile 되어야 하는데,
+   * gatsby의 webpack babel 설정에는 경로에 node_modules가 포함되어 있으면
+   * tranpiling 대상에서 제외되어 빌드 오류가 발생한다.
+   *
+   * webpack config를 수정하여 .cache 내의 파일들도 transpiling 되도록 한다.
+   */
+  config.module.rules = [
+    ...config.module.rules.filter(
+      (rule) => String(rule.test) !== String(/\.(js|mjs|jsx)$/)
+    ),
+    {
+      ...loaders.js(),
+      test: /\.(js|mjs|jsx)$/,
+      exclude: function (modulePath) {
+        if (/\.cache/.test(modulePath)) {
+          return false;
+        }
+
+        return /node_modules/.test(modulePath);
+      },
+    },
+  ];
+
+  actions.replaceWebpackConfig(config);
+};
+
 exports.onPreInit = async ({ actions, store }) => {
   const cliCwd = await getCliCwd();
 
