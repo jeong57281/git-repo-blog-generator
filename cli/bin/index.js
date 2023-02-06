0da0e615089da5fe530f8993351a9190e7bbd453
jeong57281 <jeong5728@gmail.com>
2023-02-07 01:58:05 +0900

    refactor: cli 작업 함수 분리 후 switch 문으로 정리

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -11,43 +11,46 @@ const getRepoName = require('git-repo-name');
 const constants_1 = require("../constants");
 const utils_1 = require("../utils");
 const rootPath = path_1.default.resolve(__dirname, '..', '..');
+const gatsbyCliPath = path_1.default.resolve(rootPath, 'node_modules', '.bin', 'gatsby');
+const gatsbyBuild = async () => {
+    const cliCwd = await (0, utils_1.getCliCwd)();
+    const buildCmd = (0, child_process_1.spawn)(gatsbyCliPath, ['build', '--prefix-paths'], {
+        stdio: 'inherit',
+        cwd: rootPath,
+        env: {
+            ...process.env,
+            PREFIX_PATHS: `/${getRepoName.sync(cliCwd)}/dist`,
+        },
+    });
+    buildCmd.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+            console.log(constants_1.E_NO_GATSBY_CLI);
+        }
+    });
+};
+const gatsbyDevelop = async () => {
+    const devCmd = (0, child_process_1.spawn)(gatsbyCliPath, ['develop'], {
+        stdio: 'inherit',
+        cwd: rootPath,
+    });
+    devCmd.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+            console.log(constants_1.E_NO_GATSBY_CLI);
+        }
+    });
+};
 // .git을 찾을 수 없을 때
 if (!(0, git_repo_info_1.default)().commonGitDir) {
     console.log(constants_1.E_NO_GIT);
     process.exit(1);
 }
-(async () => {
-    // $ grbgen build
-    if (process.argv[2] === 'build') {
-        const cliCwd = await (0, utils_1.getCliCwd)();
-        const buildCmd = (0, child_process_1.spawn)(path_1.default.resolve(rootPath, 'node_modules', '.bin', 'gatsby'), ['build', '--prefix-paths'], {
-            stdio: 'inherit',
-            cwd: rootPath,
-            env: {
-                ...process.env,
-                PREFIX_PATHS: `/${getRepoName.sync(cliCwd)}/dist`,
-            },
-        });
-        buildCmd.on('error', (err) => {
-            if (err.code === 'ENOENT') {
-                console.log(constants_1.E_NO_GATSBY_CLI);
-            }
-        });
-    }
-    // $ grbgen develop
-    else if (process.argv[2] === 'develop') {
-        const devCmd = (0, child_process_1.spawn)(path_1.default.resolve(rootPath, 'node_modules', '.bin', 'gatsby'), ['develop'], {
-            stdio: 'inherit',
-            cwd: rootPath,
-        });
-        devCmd.on('error', (err) => {
-            if (err.code === 'ENOENT') {
-                console.log(constants_1.E_NO_GATSBY_CLI);
-            }
-        });
-    }
-    // 잘못된 사용
-    else {
+switch (process.argv[2]) {
+    case 'build':
+        gatsbyBuild();
+        break;
+    case 'develop':
+        gatsbyDevelop();
+        break;
+    default:
         console.log(constants_1.E_USAGE);
-    }
-})();
+}
--- src/bin/index.ts
+++ src/bin/index.ts
@@ -9,6 +9,39 @@ import { E_NO_GIT, E_NO_GATSBY_CLI, E_USAGE } from '../constants';
 import { getCliCwd } from '../utils';
 
 const rootPath = path.resolve(__dirname, '..', '..');
+const gatsbyCliPath = path.resolve(rootPath, 'node_modules', '.bin', 'gatsby');
+
+const gatsbyBuild = async () => {
+  const cliCwd = await getCliCwd();
+
+  const buildCmd = spawn(gatsbyCliPath, ['build', '--prefix-paths'], {
+    stdio: 'inherit',
+    cwd: rootPath,
+    env: {
+      ...process.env,
+      PREFIX_PATHS: `/${getRepoName.sync(cliCwd)}/dist`,
+    },
+  });
+
+  buildCmd.on('error', (err: NodeJS.ErrnoException) => {
+    if (err.code === 'ENOENT') {
+      console.log(E_NO_GATSBY_CLI);
+    }
+  });
+};
+
+const gatsbyDevelop = async () => {
+  const devCmd = spawn(gatsbyCliPath, ['develop'], {
+    stdio: 'inherit',
+    cwd: rootPath,
+  });
+
+  devCmd.on('error', (err: NodeJS.ErrnoException) => {
+    if (err.code === 'ENOENT') {
+      console.log(E_NO_GATSBY_CLI);
+    }
+  });
+};
 
 // .git을 찾을 수 없을 때
 if (!getRepoInfo().commonGitDir) {
@@ -16,49 +49,13 @@ if (!getRepoInfo().commonGitDir) {
   process.exit(1);
 }
 
-(async () => {
-  // $ grbgen build
-  if (process.argv[2] === 'build') {
-    const cliCwd = await getCliCwd();
-
-    const buildCmd = spawn(
-      path.resolve(rootPath, 'node_modules', '.bin', 'gatsby'),
-      ['build', '--prefix-paths'],
-      {
-        stdio: 'inherit',
-        cwd: rootPath,
-        env: {
-          ...process.env,
-          PREFIX_PATHS: `/${getRepoName.sync(cliCwd)}/dist`,
-        },
-      }
-    );
-
-    buildCmd.on('error', (err: NodeJS.ErrnoException) => {
-      if (err.code === 'ENOENT') {
-        console.log(E_NO_GATSBY_CLI);
-      }
-    });
-  }
-  // $ grbgen develop
-  else if (process.argv[2] === 'develop') {
-    const devCmd = spawn(
-      path.resolve(rootPath, 'node_modules', '.bin', 'gatsby'),
-      ['develop'],
-      {
-        stdio: 'inherit',
-        cwd: rootPath,
-      }
-    );
-
-    devCmd.on('error', (err: NodeJS.ErrnoException) => {
-      if (err.code === 'ENOENT') {
-        console.log(E_NO_GATSBY_CLI);
-      }
-    });
-  }
-  // 잘못된 사용
-  else {
+switch (process.argv[2]) {
+  case 'build':
+    gatsbyBuild();
+    break;
+  case 'develop':
+    gatsbyDevelop();
+    break;
+  default:
     console.log(E_USAGE);
-  }
-})();
+}
