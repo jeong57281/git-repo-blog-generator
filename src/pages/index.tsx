2ced8940a46fc7337f29eae93397c88abd7b708b
jeong57281 <jeong5728@gmail.com>
2023-02-22 02:59:06 +0900

    chore: 구현한 컴포넌트 테스트 & graphql query 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import { graphql, PageProps } from 'gatsby';
+import DonutChart from '@components/DonutChart';
+import Card from '@components/Card';
+
+import styled, { css } from 'styled-components';
+import { mixin } from '@styles';
 
 interface IndexPageProps {
   site: {
@@ -9,12 +14,11 @@ interface IndexPageProps {
     };
   };
   allFile: {
-    edges: {
-      node: {
-        name: string;
-        relativePath: string;
-      };
-    };
+    nodes: {
+      name: string;
+      relativePath: string;
+      ext: string;
+    }[];
   };
   pageContext: any;
 }
@@ -26,17 +30,47 @@ function Index({
   console.log('page context', pageContext);
   console.log('graph ql filesystem', allFile);
 
+  const tmp = new Map<string, number>();
+
+  allFile.nodes.forEach(({ name, relativePath, ext }) => {
+    const count = tmp.get(ext);
+
+    if (typeof count !== 'number') {
+      tmp.set(ext, 1);
+    } else {
+      tmp.set(ext, count + 1);
+    }
+  });
+
+  const labels = [...tmp.keys()];
+  const series = [...tmp.values()];
+
   return (
     <main>
-      <h1>{site.siteMetadata.siteName}</h1>
-      <p className="custom-text">
-        This example is hosted on{' '}
-        <a href={site.siteMetadata.sourceUrl}>GitHub</a>. Continue reading{' '}
-        <a href="https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/">
-          TypeScript and Gatsby documentation
-        </a>{' '}
-        to learn more.
-      </p>
+      <ColumnBox>
+        <RowBox>
+          <Card title="언어 분포" maxWidth="33%">
+            <DonutChart labels={labels} series={series} />
+          </Card>
+          <Card title="활동량 그래프" maxWidth="66%"></Card>
+        </RowBox>
+        <RowBox>
+          <Card>
+            <div>
+              <h1>{site.siteMetadata.siteName}</h1>
+              <p className="custom-text">
+                This example is hosted on{' '}
+                <a href={site.siteMetadata.sourceUrl}>GitHub</a>. Continue
+                reading{' '}
+                <a href="https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/">
+                  TypeScript and Gatsby documentation
+                </a>{' '}
+                to learn more.
+              </p>
+            </div>
+          </Card>
+        </RowBox>
+      </ColumnBox>
     </main>
   );
 }
@@ -52,12 +86,31 @@ export const pageQuery = graphql`
       }
     }
     allFile {
-      edges {
-        node {
-          name
-          relativePath
-        }
+      nodes {
+        name
+        relativePath
+        ext
       }
     }
   }
+`;
+
+const ColumnBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  gap: 1rem;
+`;
+
+const RowBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  gap: 1rem;
+
+  ${mixin.mobile(css`
+    flex-direction: column;
+  `)}
 `;
