cd08b7ba3299af4b2730cab121403f101c291930
jeong57281 <jeong5728@gmail.com>
2023-03-04 15:16:10 +0900

    chore: 불필요한 로직 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,6 @@ import WeeklyActivityChart from '@components/Chart/WeeklyActivityChart';
 import styled from 'styled-components';
 
 interface IndexPageProps {
-  site: {
-    siteMetadata: {
-      siteName: string;
-      sourceUrl: string;
-    };
-  };
   allFile: {
     nodes: {
       name: string;
@@ -28,46 +22,30 @@ interface IndexPageProps {
       };
     }[];
   };
-  pageContext: any;
 }
 
-function Index({
-  pageContext,
-  data: { site, allFile },
-}: PageProps<IndexPageProps>) {
-  console.log('page context', pageContext);
-  console.log('graph ql filesystem', allFile);
-
+function Index({ data: { allFile } }: PageProps<IndexPageProps>) {
   /**
    * 확장자 파일 개수 count
    */
-  const tmp = new Map<string, number>();
+  const countOfExts = new Map<string, number>();
 
-  allFile.nodes.forEach(({ name, relativePath, ext }) => {
-    const count = tmp.get(ext);
-
-    if (typeof count !== 'number') {
-      tmp.set(ext, 1);
-    } else {
-      tmp.set(ext, count + 1);
-    }
+  allFile.nodes.forEach(({ ext }) => {
+    const count = countOfExts.get(ext);
+    countOfExts.set(ext, count ? count + 1 : 1);
   });
-
-  const labels = [...tmp.keys()];
-
-  const series = [...tmp.values()];
 
   /**
    * 날짜별 파일 개수 count
    */
   const countOfDates = new Map<string, number>();
 
-  allFile.nodes.forEach((value) => {
-    if (!value.fields.stampObject) {
+  allFile.nodes.forEach(({ fields }) => {
+    if (!fields.stampObject) {
       return;
     }
 
-    const date = new Date(value.fields.stampObject.created * 1000);
+    const date = new Date(fields.stampObject.created * 1000);
     const key = date.toDateString();
     const count = countOfDates.get(key);
     countOfDates.set(key, count ? count + 1 : 1);
@@ -77,26 +55,16 @@ function Index({
     <main>
       <ContentBox>
         <Card title="언어 분포" maxWidth="33.3%">
-          <DonutChart labels={labels} series={series} />
+          <DonutChart
+            labels={[...countOfExts.keys()]}
+            series={[...countOfExts.values()]}
+          />
         </Card>
         <Card title="주간 활동량" maxWidth="66.6%">
           <WeeklyActivityChart countOfDates={countOfDates} />
         </Card>
         <Card>
           <HeatmapChart countOfDates={countOfDates} />
-        </Card>
-        <Card>
-          <div>
-            <h1>{site.siteMetadata.siteName}</h1>
-            <p className="custom-text">
-              This example is hosted on{' '}
-              <a href={site.siteMetadata.sourceUrl}>GitHub</a>. Continue reading{' '}
-              <a href="https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/">
-                TypeScript and Gatsby documentation
-              </a>{' '}
-              to learn more.
-            </p>
-          </div>
         </Card>
       </ContentBox>
     </main>
@@ -107,12 +75,6 @@ export default Index;
 
 export const pageQuery = graphql`
   query IndexQuery {
-    site {
-      siteMetadata {
-        siteName
-        sourceUrl
-      }
-    }
     allFile {
       nodes {
         name
