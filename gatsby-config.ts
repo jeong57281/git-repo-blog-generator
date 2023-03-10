1bb0d9236d308db43570e5746d2def6a33d9e549
jeong57281 <jeong5728@gmail.com>
2023-03-10 14:34:36 +0900

    chore: gatsby config 플러그인 적용
    plugin : gatsby-plugin-react-svg

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -43,6 +43,14 @@ const config: GatsbyConfig = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
   ],
 };
 
