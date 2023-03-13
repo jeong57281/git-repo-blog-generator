8d850f15f947f8bd523819c27ac9db9ea8e8c15b
jeong57281 <jeong5728@gmail.com>
2023-03-13 17:31:54 +0900

    feat: searchbar 컴포넌트 input에 상태 연결 및 debounce 적용

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import SearchIcon from '@assets/images/search.svg';
+
+import { useDebouncedEffect } from '@hooks/useDebouncedEffect';
+
+import * as ACTION_TYPES from '../../store/actions/actionsType';
+import Context from '../../contexts/context';
+
 import { SearchBarLayout, SearchBarInput } from './styles';
 
 function SearchBar() {
+  const [word, setWord] = useState<string>('');
+
+  const { dispatchFilterReducer } = useContext(Context);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWord(e.target.value);
+  };
+
+  useDebouncedEffect(
+    () => {
+      dispatchFilterReducer({
+        type: ACTION_TYPES.UPDATE_FILTER.CHANGE_KEYWORD,
+        keyword: word,
+      });
+    },
+    [word],
+    1000
+  );
+
   return (
     <SearchBarLayout>
       <SearchIcon />
-      <SearchBarInput type="text" />
+      <SearchBarInput type="text" value={word} onChange={handleInputChange} />
     </SearchBarLayout>
   );
 }
